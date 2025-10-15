87295d29941faabd8b534df99a7d1dc1da85a7e2
minhsun-c <chmh0624@gmail.com>
2025-10-15 21:11:39 +0800

    Refactor UART usage to centralize through printf
    Remove direct UART function declarations from defs.h so that all
    output is routed through printf or kprintf. This ensures a single
    I/O interface for the kernel and avoids scattered use of low-level
    UART functions. Initialization of the UART is now only performed
    by start_kernel(), keeping boot responsibilities clear and well
    defined.

--- include/defs.h
+++ include/defs.h
@@ -1,12 +1,6 @@
 #ifndef __DEFS_H__
 #define __DEFS_H__
 
-/* uart */
-void uart_init();
-int uart_putc(char ch);
-void uart_puts(char *s);
-char uart_getc();
-
 /* printf */
 int kprintf(const char *s, ...);
 void panic(char *s);
